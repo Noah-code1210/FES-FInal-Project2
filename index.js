924e57e2100e1b5435e1c9d326dc565ec35b2895
Noah <noahjames1035@gmail.com>
2025-05-05 17:30:11 -0400

    JavaScript Debugging

---
+++ index.js
@@ -0,0 +1,25 @@
+const pokemonListEl = document.querySelector(".pokemon__list");
+
+
+async function main() {
+  const pokemon = await fetch("https://api.pokemontcg.io/v2/cards");
+  const pokemonData = await pokemon.json();
+  console.log(pokemonData);
+
+  pokemonListEl.innerHTML = pokemonData.map((pokemon) => userHTML(user)).join("")
+}
+
+main();
+
+function userHTML(pokemon) {
+    return `<div class="pokemon__slot">
+    <div class="pokemon__slot--description">
+        <img class="pokemon__img" src="">
+           <h3>${pokemon.name}</h3>
+             <p><b>Type:</b>${pokemon.types}</p>
+             <p><b>Sub-type</b>${pokemon.subtypes}</p>
+             <p><b>Attacks:</b>${pokemon.hp}</p>
+             <p><b>Prices:</b>${pokemon.rarity}</p>
+        </div>
+    </div>`
+}
