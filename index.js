3a12f2fc8acdfb6c412928c5e2cb97093a7b89ae
Noah <noahjames1035@gmail.com>
2025-05-06 18:38:36 -0400

    Filter JS Debugging

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const pokemonListEl = document.querySelector(".pokemon__list");
 
-async function main() {
+async function getData(filter) {
+
+  if (filter === 'LOW_TO_HIGH') {
+    console.log(filter);
+    pokemon.sort((a, b) => a.hp - b.hp)
+    console.log(filterCards)
+  }
+  
   const pokemon = await fetch("https://api.pokemontcg.io/v2/cards");
   const pokemonData = await pokemon.json();
+
   console.log(pokemonData);
   pokemonListEl.innerHTML = pokemonData.data.map((pokemon) => userHTML(pokemon)).join("");
 }
-
-main();
 
 function userHTML(pokemon) {
   return `<div class="pokemon__slot">
     <div class="pokemon__slot--description">
         <img class="pokemon__img" src=${pokemon.images.small}>
-           <h3>${pokemon.name}</h3>
-             <p><b>Type:</b> ${pokemon.types}</p>
-             <p><b>Sub-type:</b> ${pokemon.subtypes}</p>
-             <p><b>Health:</b> ${pokemon.hp}</p>
-             <p><b>Prices:</b> ${pokemon.rarity}</p>
+             <h3>${pokemon.name}</h3>
+               <p><b>Type:</b> ${pokemon.types}</p>
+               <p><b>Sub-type:</b> ${pokemon.subtypes}</p>
+               <p><b>Health:</b> ${pokemon.hp}</p>
+               <p><b>Rarity:</b> ${pokemon.rarity}</p>
         </div>
-    </div>`
+    </div>`;
 }
+
+function filterCards(event) {
+    getData(event.target.value)
+  }
+
+setTimeout(() => {
+  getData();
+})
+
